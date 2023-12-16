e48b25dc1a5b9695f5f3ebc88dfeb05a3e054d65
dekkeng <dek_keng@hotmail.com>
2023-12-16 09:42:26 +0700

    Alpaca support

--- js/contentScript.js
+++ js/contentScript.js
@@ -149,6 +149,9 @@ async function get_floor_price(filter, order = 'PRICE_ASC') {
         case 'shibainu':
             str = "propertyIds=16633703&"
             break;
+        case 'alpaca':
+            str = "propertyIds=17241718&"
+            break;
 
         default:
             str = filter
@@ -394,6 +397,18 @@ function prepare_container() {
                     "<td align='right' class='floor'>"+loading_image+"</td>"+
                     "<td align='right' class='usd'>"+loading_image+"</td>"+
                 "</tr>"+
+                "<tr id='dk_floor_alpaca'>"+
+                    "<td align='left'>"+
+                      "<a href='https://citizen.store.dosi.world/en-US/marketplace?pageNo=1&collectibleId=2&propertyIds=17241718&category=&nftOrder=PRICE_ASC&currency=' target='_blank'>"+
+                          "Alpaca"+
+                      "</a>"+
+                    "</td>"+
+                    "<td align='right' class='owned'>"+loading_image+"</td>"+
+                    "<td align='right' class='selling'>"+loading_image+"</td>"+
+                    "<td align='right' class='market'>"+loading_image+"</td>"+
+                    "<td align='right' class='floor'>"+loading_image+"</td>"+
+                    "<td align='right' class='usd'>"+loading_image+"</td>"+
+                "</tr>"+
             "</tbody>"+
           "</table>");
 }
@@ -441,6 +456,7 @@ async function generate_dosi_report(url = '') {
       gen_floor_price_text(data.summary['Cockatiel'], selling.summary['Cockatiel'], "lutino");
       gen_floor_price_text(data.summary['Robo Rex'], selling.summary['Robo Rex'], "roborex");
       gen_floor_price_text(data.summary['Shiba Inu'], selling.summary['Shiba Inu'], "shibainu");
+      gen_floor_price_text(data.summary['Alpaca'], selling.summary['Alpaca'], "alpaca");
   }
 }
 
