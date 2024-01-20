5b681e03f532a84cc232efa1c0e75c0f5a41dcfe
dekkeng <dek_keng@hotmail.com>
2024-01-20 09:59:51 +0700

    Add bear

--- js/contentScript.js
+++ js/contentScript.js
@@ -152,6 +152,9 @@ async function get_floor_price(filter, order = 'PRICE_ASC') {
         case 'alpaca':
             str = "propertyIds=17241718&"
             break;
+        case 'glizzly':
+            str = "propertyIds=17697031&"
+            break;
 
         default:
             str = filter
@@ -409,6 +412,18 @@ function prepare_container() {
                     "<td align='right' class='floor'>"+loading_image+"</td>"+
                     "<td align='right' class='usd'>"+loading_image+"</td>"+
                 "</tr>"+
+                "<tr id='dk_floor_glizzly'>"+
+                    "<td align='left'>"+
+                      "<a href='https://citizen.store.dosi.world/en-US/marketplace?pageNo=1&collectibleId=2&propertyIds=17697031&category=&nftOrder=PRICE_ASC&currency=' target='_blank'>"+
+                          "Glizzly"+
+                      "</a>"+
+                    "</td>"+
+                    "<td align='right' class='owned'>"+loading_image+"</td>"+
+                    "<td align='right' class='selling'>"+loading_image+"</td>"+
+                    "<td align='right' class='market'>"+loading_image+"</td>"+
+                    "<td align='right' class='floor'>"+loading_image+"</td>"+
+                    "<td align='right' class='usd'>"+loading_image+"</td>"+
+                "</tr>"+
             "</tbody>"+
           "</table>");
 }
@@ -457,6 +472,7 @@ async function generate_dosi_report(url = '') {
       gen_floor_price_text(data.summary['Robo Rex'], selling.summary['Robo Rex'], "roborex");
       gen_floor_price_text(data.summary['Shiba Inu'], selling.summary['Shiba Inu'], "shibainu");
       gen_floor_price_text(data.summary['Alpaca'], selling.summary['Alpaca'], "alpaca");
+      gen_floor_price_text(data.summary['Glizzly'], selling.summary['Glizzly'], "glizzly");
   }
 }
 
